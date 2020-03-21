667a37dbf03b6df4abf23cf7fed7899f7555612b
Brentonius <bjavonjackson@gmail.com>
2020-03-21 02:59:53 -0400

    updated portfolio

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends React.Component {
 	render() {
 		return (
 		    <section className="foot">
-		        <p>Created by Brenton Jackson <a href="https://github.com/brentonjackson/portfolio" className="github-link" target="_blank">
+		        <p>Created by Brenton Jackson <a href="https://github.com/brentonjackson/reactportfolio" className="github-link" target="_blank">
 		        <i className="fab fa-github github-icon fa-lg"></i></a></p>
 		    </section>
 
