467ccc46558422c7a1efaa8a67c4e6ce8a3c665e
Brentonius <bjavonjackson@gmail.com>
2020-03-20 22:42:42 -0400

    deploying

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import About from './About.js';
