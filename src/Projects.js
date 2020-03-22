ae2a5db4117742180852403d92a6364b303020a8
Brentonius <bjavonjackson@gmail.com>
2020-03-21 23:30:36 -0400

    added meta tags and updated project descriptions

--- src/Projects.js
+++ src/Projects.js
@@ -18,10 +18,9 @@ class Projects extends React.Component {
 						<h2>Portfolio Website</h2><br/><br/>
 						<p>React, HTML, CSS, JavaScript, jQuery, Animate.css</p><br /><br/>
 						<h3>Summary</h3>
-						<p className="summary">This site was my first shipped site in React. I wanted to make my site easier to update and ship by piecing together components. <br/>
-							Firsts: Learning how to create React components, pass props, use JSX, React Router, and npm extensively.<br/> 
-							I learned how to create React apps from scratch as well as convert pre-existing sites, as well as a lot about deployment while doing this. <br/>
-							In learning the basics of React, this was a success!</p>
+						<p className="summary">Refactored portfolio in React. 
+							I learned how to create React apps from scratch as well as refactor pre-existing HTML sites. Also learned a lot about deployment while doing this.
+						</p>
 					</div>
 
 			    	<Thumbnail
@@ -33,11 +32,11 @@ class Projects extends React.Component {
 						<h2>Landing Page Redesign</h2><br/><br/>
 						<p>HTML, CSS, JavaScript, jQuery, Animate.css</p><br /><br/>
 						<h3>Summary</h3>
-						<p className="summary">This project was to see how I could improve the UI and overall experience (UX) of a startup's landing page. <br/>
-							Firsts: Using animations to keep the attention of a younger target audience, using gradients to help with visual hierarchy, 
-							and being minimal to reduce confusion by the end user. <br/>
+						<p className="summary">Improved the UI and overall experience (UX) of a startup's landing page. <br/>
+							Used animations to keep the attention of a younger target audience, gradients to help with visual hierarchy, 
+							and reduced distraction of the user by being minimal in my design approach. <br/>
 							Presenting to an actual client made me cognizant about the
-							presentation of my work and improved my skills creating mockups. My client loved the end result!</p>
+							presentation of my work and improved my mockup skills. My client loved the end result!</p>
 					</div>
 					<Thumbnail
 			    		link="https://github.com/brentonjackson/clarion"
@@ -48,8 +47,8 @@ class Projects extends React.Component {
 						<h2>Mobile App Design</h2><br/><br/>
 						<p>Designed/ prototyped with Photoshop, Adobe XD</p><br/><br/>
 						<h3>Summary</h3>
-						<p className="summary">This was a project that I did for a make-a-thon. My goal was to simplify the decision users had to make when deciding how to travel anywhere.
-						It helped me improve my prototyping skills and stretched my abilities using Adobe XD. The project also piqued my interest in REST APIs! Project coming soon! 
+						<p className="summary">Goal: To simplify the decision users had to make when deciding how to travel anywhere.
+						This helped me improve my prototyping skills and stretched my abilities using Adobe XD. Also piqued my interest in REST APIs 
 						</p>
 					</div>
 					<Thumbnail
@@ -61,8 +60,8 @@ class Projects extends React.Component {
 						<h2>Video Background Bio Page</h2><br/><br/>
 						<p>HTML, CSS, JavaScript					</p><br/><br/>
 						<h3>Summary</h3>
-						<p className="summary">I made this project learning HTML and CSS. I played around with CSS frameworks like Bootstrap and W3.CSS.
-						It also helped me grasp Responsive Design and more advanced CSS concepts.</p>
+						<p className="summary">One of my firsts using HTML and CSS. I played around with CSS frameworks like Bootstrap and W3.CSS.
+						This helped me grasp Responsive Design and more advanced CSS concepts.</p>
 					</div>
 				</div>
 		    </div>
