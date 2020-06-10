cbfc871f4c2aa686cba9666d092cf9fdb332956a
Brentonius <bjavonjackson@gmail.com>
2020-06-10 01:11:44 -0400

    edit mobile resume font weight

--- src/Resume.js
+++ src/Resume.js
@@ -16,13 +16,13 @@ class Resume extends React.Component {
                     </ul>
                     <h3 className="section-header">Skills</h3>
                         <ul className="skills">
-                            <li>Languages: JavaScript, Java, MATLAB, C++</li>
-                            <li>Technologies: React, MongoDB, Express, Node, HTML5, CSS3, jQuery, Bootstrap, git</li>
+                            <li>Languages: <strong>JavaScript</strong>, Java, MATLAB, C++</li>
+                            <li>Technologies: <strong>React, MongoDB, Express, Node, HTML5, CSS3, jQuery, Bootstrap, git</strong></li>
                             <li>Design: Research, Wireframing, Digital mockups</li>
                         </ul>
                     <h3 className="section-header">Education</h3>
                         <ul className="education">
-                            <li className="content-header">Georgia Institute of Technology, Industrial Design - 2017-2020</li>
+                            <li className="content-header"><strong>Georgia Institute of Technology,</strong> Industrial Design - 2017-2020</li>
                                 <ul className="content">
                                     <li>Extensive Coursework in Computer Engineering and Industrial Design</li>
                                 </ul>
@@ -39,18 +39,18 @@ class Resume extends React.Component {
                     <ul className="recent-projects">
                         <li className="content-header"><a href="https://github.com/brentonjackson/mern-workout-app" target="_blank" rel="noopener noreferrer">MERN Workout Tracker</a></li>
                             <ul className="content">
-                                <li>React, MongoDB, Express, Node, Auth0</li>
+                                <li><strong>React, MongoDB, Express, Node,</strong> Auth0</li>
                                 <li>Designed/launched web app that allows users to keep track of their workouts and provides motivation to persevere</li>
                                 <li>Designed/implemented API to allow user to save, delete, update, and create new workouts</li>
                             </ul>
                         <li className="content-header"><a href="https://brentonjackson.github.io/fitnessgram" target="_blank" rel="noopener noreferrer">FitnessGram</a></li>
                             <ul className="content">
-                                <li>JavaScript, CSS3, HTML</li>
-                                <li>  Developed site to help motivate users and hold themselves accountable while working out</li>
+                                <li><strong>JavaScript, CSS3, HTML5</strong></li>
+                                <li>Developed site to help motivate users and hold themselves accountable while working out</li>
                             </ul>
                         <li className="content-header"><a href="https://brentonjackson.me" target="_blank" rel="noopener noreferrer">Personal Portfolio</a></li>
                             <ul className="content">
-                                <li>React, JavaScript, jQuery, Animate.css, HTML, CSS</li>
+                                <li><strong>React, JavaScript, jQuery,</strong> Animate.css, HTML, CSS</li>
                                 <li>  Designed/launched responsive website to showcase projects</li>
                             </ul>
                         <li className="content-header"><a href="https://github.com/brentonjackson/Flashcard-Android-App" target="_blank" rel="noopener noreferrer">Flashcards Android App</a></li>
