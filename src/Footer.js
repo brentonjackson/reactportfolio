8db184a3e025460290510976adc81aa7e52c17c9
Brentonius <bjavonjackson@gmail.com>
2020-06-11 22:23:07 -0400

    changed link to github

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
 	render() {
 		return (
 		    <section className="foot">
-		        <p>© Brenton Jackson <a href="https://github.com/brentonjackson/reactportfolio" className="github-link" target="_blank" rel="noopener noreferrer">
+		        <p>© Brenton Jackson <a href="https://github.com/brentonjackson/" className="github-link" target="_blank" rel="noopener noreferrer">
 		        <i className="fab fa-github github-icon fa-lg"></i></a></p>
 		    </section>
 
