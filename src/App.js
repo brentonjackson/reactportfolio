4325f693fa38b323090f81cf949afe10d5a5c7a3
Brentonius <bjavonjackson@gmail.com>
2020-07-17 17:29:51 -0400

    fixed mobile resume

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {Suspense, lazy} from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
+import "./app.css";
+import { AnimatePresence } from "framer-motion"
+
+// const Navbar = lazy(() => import('./Navbar'));
+// const Footer = lazy(() => import('./Footer'));
+// const About = lazy(() => import('./About'));
+// const Projects = lazy(() => import('./Projects'));
+// const Resume = lazy(() => import('./Resume'));
+// const Contact = lazy(() => import('./Contact'));
+
+
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import About from "./About.js";
 import Projects from "./Projects.js";
 import Resume from "./Resume.js";
 import Contact from "./Contact.js";
-import "./app.css";
-import { AnimatePresence } from "framer-motion"
+
 
 
 
@@ -18,12 +28,14 @@ function App() {
       <>
         <Navbar />
         <AnimatePresence exitBeforeEnter initial={false}>
-          <Switch location={location} >
-            <Route path="/projects" component={Projects} exact />
-            <Route path="/resume" component={Resume} exact />
-            <Route path="/contact" component={Contact} exact />
-            <Route path="/" component={About} exact />
-          </Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch location={location}>
+              <Route path="/projects" component={Projects} exact />
+              <Route path="/resume" component={Resume} exact />
+              <Route path="/contact" component={Contact} exact />
+              <Route path="/" component={About} exact />
+            </Switch>
+          </Suspense>
         </AnimatePresence>
         
         <Footer />
