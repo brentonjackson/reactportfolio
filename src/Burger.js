c53d9383f1fd020b32cbfe6614b487108925b619
Brentonius <bjavonjackson@gmail.com>
2021-04-09 14:09:46 -0400

    updated resume

--- src/Burger.js
+++ src/Burger.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import { bool, func } from 'prop-types';
-import {StyledBurger} from './Burger.styled';
-import './burger.css';
+import React from "react";
+import { StyledBurger } from "./Burger.styled";
+import "./burger.css";
 
-const Burger = ({open, setOpen}) => {
-    return (
-        <div className="burger-nav">
-            <StyledBurger className="styled-button" open={open} onClick={() => setOpen(!open)}>
-                <div className="burger" />
-                <div className="burger"  />
-                <div className="burger" />
-            </StyledBurger>
-        </div>
-        
-        // <section className="burger-nav" open={open} onClick={() => setOpen(!open)}>
-        //     <div className="styled-button">
-                // <div className="burger"></div>
-                // <div className="burger"></div>
-                // <div className="burger"></div>
-        //     </div>
-        // </section>
-    )
-}
-Burger.propTypes = {
-    open: bool.isRequired,
-    setOpen: func.isRequired,
+const Burger = ({ open, setOpen }) => {
+  return (
+    <div className="burger-nav">
+      <StyledBurger
+        className="styled-button"
+        open={open}
+        onClick={() => setOpen(!open)}
+      >
+        <div className="burger" />
+        <div className="burger" />
+        <div className="burger" />
+      </StyledBurger>
+    </div>
+  );
 };
-  export default Burger;
+
+export default Burger;
--- src/Menu.styled.js
+++ src/Menu.styled.js
@@ -1,12 +1,12 @@
 // Menu.styled.js
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledMenu = styled.nav`
   display: flex;
   flex-direction: column;
   justify-content: center;
   background: var(--blue-gradient);
-//   height: 100vh;
+  //   height: 100vh;
   text-align: left;
   padding-top: 40px;
   position: absolute;
@@ -16,13 +16,12 @@ export const StyledMenu = styled.nav`
   transition: transform 0.3s ease-in-out;
   width: 100%;
   z-index: 5;
-  transform: ${({ open }) => open ? 'translateY(4%)' : 'translateY(-100%)'};
-  
+  transform: ${({ open }) => (open ? "translateY(4%)" : "translateY(-100%)")};
 
   a {
     font-size: 2rem;
     text-transform: uppercase;
-    font-family: 'Montserrat';
+    font-family: "Montserrat";
     padding: 2rem 0;
     color: var(--main-white);
     text-decoration: none;
